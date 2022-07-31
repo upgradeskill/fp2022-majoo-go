81fa48b7fa1491de032cfd2e3bf46bb0e9a79df9
eka pratama <ekapratama@ekas-MacBook-Air.local>
2022-07-31 08:48:54 +0700

    crud category

--- controllers/category_controller.go
+++ controllers/category_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	model "mini-pos/models"
 	"mini-pos/structs"
 	"net/http"
@@ -20,6 +21,74 @@ func CategoryList(c echo.Context) error {
 		response.Status = 200
 		response.Message = "Sukses melihat data"
 		response.Data = categories
+		return c.JSON(http.StatusOK, response)
+	}
+}
+
+func CategoryStore(c echo.Context) error {
+	category := new(structs.Categories)
+	c.Bind(category)
+	contentType := c.Request().Header.Get("Content-type")
+	if contentType == "application/json" {
+		fmt.Println("Request dari json")
+	}
+	response := new(structs.Response)
+
+	if model.CreateCategory(category) != nil { // method create category
+		response.Status = 500
+		response.Message = "Gagal create data"
+		return c.JSON(http.StatusInternalServerError, response)
+	} else {
+		response.Status = 200
+		response.Message = "Sukses create data"
+		response.Data = *category
+		return c.JSON(http.StatusOK, response)
+	}
+}
+
+func CategoryShow(c echo.Context) error {
+	category, err := model.GetOneCategoryById(c.Param("id")) // method get by email
+	response := new(structs.Response)
+
+	if err != nil {
+		response.Status = 404
+		response.Message = "Data tidak ditemukan"
+		return c.JSON(http.StatusNotFound, response)
+	} else {
+		response.Status = 200
+		response.Message = "Sukses melihat data"
+		response.Data = category
+		return c.JSON(http.StatusOK, response)
+	}
+}
+
+func CategoryUpdate(c echo.Context) error {
+	category := new(structs.Categories)
+	c.Bind(category)
+	response := new(structs.Response)
+	if model.UpdateCategory(c.Param("id"), category) != nil { // method update category
+		response.Status = 500
+		response.Message = "Gagal update data"
+		return c.JSON(http.StatusInternalServerError, response)
+	} else {
+		response.Status = 200
+		response.Message = "Sukses update data"
+		response.Data = *category
+		return c.JSON(http.StatusOK, response)
+	}
+}
+
+func CategoryDelete(c echo.Context) error {
+	category, _ := model.GetOneCategoryById(c.Param("id"))
+	response := new(structs.Response)
+
+	if model.DeleteCategory(&category) != nil {
+		response.Status = 404
+		response.Message = "data tidak ditemukan"
+		return c.JSON(http.StatusNotFound, response)
+	} else {
+		response.Status = 200
+		response.Message = "Sukses menghapus data"
 		return c.JSON(http.StatusOK, response)
 	}
 }
